ad99df70c55f0a8152cb8a6364a74674fc3b5f55
Kapil Kale <kapil.kale.07@gmail.com>
2015-11-14 14:00:00 -0500

    remove extra code

--- app.rb
+++ app.rb
@@ -24,13 +24,3 @@ class App < Sinatra::Base
 
   run! if app_file == $0
 end
-
-# # Blog stuff.
-# get '/blog/?' do
-#   File.read(File.join('public/blog/index.html'))
-# end
-
-# get '/blog/:post/?' do
-#   File.read(File.join('public/blog', "#{params[:post]}", "index.html"))
-# end
-
